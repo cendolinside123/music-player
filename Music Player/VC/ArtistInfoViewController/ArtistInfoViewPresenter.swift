36965c9bc4a9452ac4ed2462f849345559b7c874
jan <jnsbstn391@gmail.com>
2021-06-10 22:17:44 +0700

    add file: - ArtistInfoViewController -ArtistInfoViewPresenter -ArtistInfoViewPresenterRule

---
+++ Music Player/VC/ArtistInfoViewController/ArtistInfoViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ArtistInfoViewPresenter.swift
+//  Music Player
+//
+//  Created by Mac on 10/06/21.
+//
+
+import Foundation
---
+++ Music Player/VC/ArtistInfoViewController/ArtistInfoViewPresenterRule.swift
@@ -0,0 +1,13 @@
+//
+//  ArtistInfoViewPresenterRule.swift
+//  Music Player
+//
+//  Created by Mac on 10/06/21.
+//
+
+import Foundation
+
+
+protocol ArtistInfoViewPresenterRule {
+    
+}
