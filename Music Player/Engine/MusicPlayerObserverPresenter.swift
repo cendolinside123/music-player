6e3f4146fe16e1a5d5f3a3466457df4e5f48915c
jan <jnsbstn391@gmail.com>
2021-06-10 20:52:12 +0700

    remove unused commen

--- Music Player/Engine/MusicPlayerObserverPresenter.swift
+++ Music Player/Engine/MusicPlayerObserverPresenter.swift
@@ -39,19 +39,6 @@ extension MusicPlayerObserverPresenter: MusicPlayerObserverPresenterRule {
                     controller?.playbackLikelyToKeepUp_loading()
                 }
             case "duration":
-//                if CMTIME_IS_INDEFINITE(item.duration) || item.duration != .zero {
-//                    if let timeObserver = controller?.getTimeObserver() {
-//
-//                        controller?.setTimeObserver(timeObserver: nil)
-//
-//                    }
-//                }
-//                else {
-//                    let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-//                    let duration = Double(CMTimeGetSeconds(item.duration))
-//                    controller?.doUpdateDuration(duration: duration)
-//                    controller?.setTimeObserver(interval: interval)
-//                }
                 let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
                 let duration = Double(item.duration.seconds)
                 controller?.doUpdateDuration(duration: duration)
