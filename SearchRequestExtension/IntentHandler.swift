53993b487c14325fd04f89a77d0f6d656ad25a0f
jan <jnsbstn391@gmail.com>
2021-06-25 16:52:25 +0700

    add handle siri

--- SearchRequestExtension/IntentHandler.swift
+++ SearchRequestExtension/IntentHandler.swift
@@ -9,13 +9,18 @@ import Intents
 
 class IntentHandler: INExtension {
     
+    lazy var coreData = CoreDataStack(modelName: "Music_Player")
+    
     override func handler(for intent: INIntent) -> Any {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
         
         if #available(iOSApplicationExtension 12.0, *) {
             if intent is INPlayMediaIntent {
-                return SearchRequestHandler()
+                return SearchRequestHandler(coreData: coreData)
+            }
+            else if intent is INUpdateMediaAffinityIntent {
+                return SearchRequestHandler(coreData: coreData)
             }
             else if intent is INSetAudioSourceInCarIntent {
                 return self
--- SearchRequestExtension/SearchRequestHandler.swift
+++ SearchRequestExtension/SearchRequestHandler.swift
@@ -9,9 +9,25 @@ import UIKit
 import Intents
 
 class SearchRequestHandler: NSObject,INPlayMediaIntentHandling {
+    
+    private var coreDataStack: CoreDataStack? = nil
+    private var queue: Queue? = nil
+    private var coreDataMusic: CoreDataMusicProtocol? = nil
 
     override init() {
         
+    }
+    convenience init(coreData: CoreDataStack) {
+        self.init()
+        coreDataStack = coreData
+        
+        queue = Queue(coreData: coreDataStack)
+        coreDataMusic = CoreDataMusic()
+    }
+    
+    
+    func setCoreDataStack(coreData: CoreDataStack) {
+        coreDataStack = coreData
     }
     
     @available(iOSApplicationExtension 12.0, *)
@@ -25,16 +41,54 @@ class SearchRequestHandler: NSObject,INPlayMediaIntentHandling {
     func resolveMediaItems(for intent: INPlayMediaIntent, with completion: @escaping ([INPlayMediaMediaItemResolutionResult]) -> Void) {
         print("\(intent.mediaSearch?.mediaName)")
         
-        let song = Repo.gallow_ParkestAlbum[1]
-        
-        MusicPlayerUtility.shared.setCurrentURL(url: song.url)
-        MusicPlayerUtility.shared.setup(getMusic: song)
-        MusicPlayerUtility.shared.play()
         
         
-        completion([INPlayMediaMediaItemResolutionResult.success(with: INMediaItem(identifier: nil, title: song.title, type: .song, artwork: nil))])
+//        let song = Repo.gallow_ParkestAlbum[1]
+//
+//        MusicPlayerUtility.shared.setCurrentURL(url: song.url)
+//        MusicPlayerUtility.shared.setup(getMusic: song)
+//        MusicPlayerUtility.shared.play()
+        
+        testPlayQueueSong(doPlay: { music in
+            
+            MusicPlayerUtility.shared.setCurrentURL(url: music.url)
+            MusicPlayerUtility.shared.setup(getMusic: music)
+            MusicPlayerUtility.shared.play()
+            
+            completion([INPlayMediaMediaItemResolutionResult.success(with: INMediaItem(identifier: nil, title: "Gallow", type: .song, artwork: nil))])
+        }, doFailed: {
+            completion([INPlayMediaMediaItemResolutionResult.unsupported(forReason: .serviceUnavailable)])
+        })
+        
+        
+        
+    }
+    
+    private func testPlayQueueSong(doPlay: @escaping (Music) -> () , doFailed: @escaping () -> ()) {
+        coreDataStack?.doInBackground(managedContext: { [weak self] context in
+            self?.coreDataMusic?.deleteAllSong(managedContext: context, success: {
+                self?.coreDataMusic?.addAllSong(managedContext: context, musics: Repo.gallow_ParkestAlbum, success: {
+                    self?.queue?.getPlayedSong(result: { result in
+                        guard let music = result else {
+                            print("failed get now playing")
+                            doFailed()
+                            return
+                        }
+                        
+                        doPlay(music)
+                        
+                    })
+                }, failed: {
+                    
+                })
+            }, failed: {
+                
+            })
+        })
     }
 
     
     
 }
+
+
